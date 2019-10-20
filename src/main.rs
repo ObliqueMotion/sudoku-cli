36ba33ff026c7c3a4ca5b57ab979afcd4846f336
Erik Nordin <erik@pop-os.localdomain>
2019-10-19 23:48:04 -0700

    Read Board From String

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 pub mod sudoku;
-
 use sudoku::board::SudokuBoard;
 
+static HARD_PUZZLE: &str = include_str!("../puzzles/hard");
+
 fn main() {
-    let board = SudokuBoard::default();
-    board.insert(5, 0, 3);
-    board.insert(7, 3, 1);
-    board.insert(2, 5, 7);
+    let board = SudokuBoard::default()
+        .insert(5, 0, 3)
+        .insert(7, 3, 1)
+        .insert(2, 5, 7);
     println!("\n{}", board);
+
+    let board2 = SudokuBoard::from(HARD_PUZZLE);
+    println!("\n{}", board2);
 }
--- src/sudoku/board.rs
+++ src/sudoku/board.rs
@@ -1,5 +1,6 @@
 use super::data::SudokuData;
 use std::fmt;
+use std::iter::repeat;
 
 #[derive(Clone, Debug, Default)]
 pub struct SudokuBoard {
@@ -27,15 +28,19 @@ fn box_index(row: usize, col: usize) -> usize {
 }
 
 impl SudokuBoard {
-    pub fn insert(&self, value: u64, row: usize, col: usize) {
+    pub fn insert(self, value: u64, row: usize, col: usize) -> Self {
+        if value == 0 {
+            return self;
+        }
         assert!((1..=9).contains(&value));
         assert!((0..=8).contains(&row));
         assert!((0..=8).contains(&col));
-        let bx = box_index(row, col);
+        let bdx = box_index(row, col);
         self.state[row].set_cell(value, col);
         self.state[row].mark_in_row(value);
         self.state[col].mark_in_col(value);
-        self.state[bx ].mark_in_box(value);
+        self.state[bdx].mark_in_box(value);
+        self
     }
 }
 
@@ -61,4 +66,32 @@ impl fmt::Display for SudokuBoard {
         writeln!(f, "  {}", self.state[8])?;
         writeln!(f, "  ╚═══════════╩═══════════╩═══════════╝")
     }
+}
+
+impl From<&str> for SudokuBoard {
+    fn from(input: &str) -> Self {
+        input
+            .trim()
+            .chars()
+            .filter(|c| !c.is_whitespace())
+            .filter_map(|c| c.to_digit(10).or(Some(0)))
+            .zip(board_indices())
+            .fold(SudokuBoard::default(), |board, (value, (row, col))| {
+                board.insert(value.into(), row, col)
+            })
+    }
+}
+
+fn board_indices() -> impl Iterator<Item = (usize, usize)> {
+    repeat(0)
+        .take(9)
+        .chain(repeat(1).take(9))
+        .chain(repeat(2).take(9))
+        .chain(repeat(3).take(9))
+        .chain(repeat(4).take(9))
+        .chain(repeat(5).take(9))
+        .chain(repeat(6).take(9))
+        .chain(repeat(7).take(9))
+        .chain(repeat(8).take(9))
+        .zip((0..9).cycle())
 }
