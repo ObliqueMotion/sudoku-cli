fc51e23cb60737e2738e9beae357aeac1f88ac42
Erik Nordin <erik@pop-os.localdomain>
2019-10-21 16:15:22 -0700

    Clean Up Main

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ static HARD_PUZZLE: &str = include_str!("../puzzles/50k");
 
 fn main() {
     let mut board = SudokuBoard::from(HARD_PUZZLE);
-    println!("\n{}", board);
+
+    println!("\n\n  Starting Board:\n\n{}", board);
+
     let now = Instant::now();
-    let solutions = board.count_all_solutions();
+    let solutions = board.count_solutions();
     let time = now.elapsed();
+
     println!("  Found: {} solution(s)", solutions);
-    println!("\n\n  {} second(s)\n\n", time.as_secs_f64())
+    println!("\n  Time:  {} second(s)\n\n", time.as_secs_f64());
 }
