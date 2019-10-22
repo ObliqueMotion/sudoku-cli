233a762597612e2a0e4546f6d3ab5e551e641629
Erik Nordin <erik@pop-os.localdomain>
2019-10-22 01:42:39 -0700

    Add basic cli functionality

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use sudoku::board::SudokuBoard;
 use structopt::StructOpt;
 use clap::arg_enum;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 
 arg_enum! {
     #[derive(Debug)]
@@ -19,36 +21,47 @@ arg_enum! {
 #[derive(StructOpt, Debug)]
 #[structopt(name = "sudoku-cli")]
 struct Opt {
-    #[structopt(short, long, default_value = "0")]
-    threads: usize,
-    #[structopt(short = "m", long = "millis-per-frame", default_value = "50")]
+    /// The number of threads to use [default: system CPU count]
+    #[structopt(short, long)]
+    threads: Option<usize>,
+    
+    /// The number of milliseconds per frame for {--action=watch} mode
+    #[structopt(short = "m", long = "millis", default_value = "50")]
     millis: u64,
 
+    /// The input path for a puzzle (required if --str is unspecified)
     #[structopt(short = "p", long = "path")]
     input_path: Option<String>,
     
+    /// The output path for {--action=write} mode
+    #[structopt(short = "o", long = "out")]
+    output_path: Option<String>,
+    
+    /// The input string for a puzzle (required if --path is unspecified)
     #[structopt(short = "s", long = "str")]
     input_str: Option<String>,
 
+    /// Toggles compact output
     #[structopt(short = "c", long = "compact")]
     compact: bool,
     
+    /// The action to perform { count, watch, display, write }
     #[structopt(short = "a", long = "action")]
     action: Action,
 }
 
 fn main() {
     let opt = Opt::from_args();
-    if opt.threads > 0 {
+    if let Some(num_threads) = opt.threads {
         ThreadPoolBuilder::default()
-            .num_threads(opt.threads)
+            .num_threads(num_threads)
             .build_global()
-            .expect(&format!("Failed to build thread pool with {} threads.", opt.threads));
+            .expect(&format!("Failed to build thread pool with {} threads.", num_threads));
     }
    
     let input = match (&opt.input_path, &opt.input_str) {
-        (None, None) => panic!("\n\nsudoku-cli error: Must specify either --input-path or --input-str\n\n"),
-        (Some(_), Some(_)) => panic!("\n\nsudoku-cli error: Must specify only one of --input-path or --input-str\n\n"),
+        (None, None) => panic!("\n\nsudoku-cli error: Must specify either --path or --str\n\n"),
+        (Some(_), Some(_)) => panic!("\n\nsudoku-cli error: Must specify only one of --path or --str\n\n"),
         (Some(path), _) => fs::read_to_string(&path).expect(&format!("\n\nsudoku-cli error: Failed to read from path {}\n\n", &path)),
         (_, Some(string)) => String::from(string),
     };
@@ -58,7 +71,7 @@ fn main() {
     match opt.action {
         Action::Watch => board.watch_find_solutions(opt.millis),
         Action::Count => {
-            println!("\n\n  Sudoku Board:\n\n{}", board);
+            println!("{}", board);
             let now = Instant::now();
             let count = board.count_solutions_par();
             let elapsed = now.elapsed();
@@ -79,7 +92,23 @@ fn main() {
             println!("  Found: {} solution(s)\n", count);
             println!("  Time:  {} second(s)\n", elapsed.as_secs_f64());
         },
-        _ => panic!(),
+        Action::Write => {
+            let path = opt.output_path.unwrap();
+            let mut file = File::create(&path).unwrap();
+            println!("\n\n  Sudoku Board:\n\n{}", board);
+            let now = Instant::now();
+            let solutions = if opt.compact {
+                board.find_solutions_compact_par()
+            } else {
+                board.find_solutions_par()
+            };
+            let elapsed = now.elapsed();
+            let count = board.count_solutions_par();
+            file.write_all(solutions.as_bytes()).unwrap();
+            println!("  Found: {} solution(s)\n", count);
+            println!("  Time:  {} second(s)\n", elapsed.as_secs_f64());
+            println!("  Writing solutions to file: {}\n\n", path);
+        },
     }
     
 }
